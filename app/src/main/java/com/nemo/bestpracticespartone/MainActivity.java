602d1c02caed14a206891903ae54059e32abe7b5
java2019 <java_2019@mail.ru>
2017-07-12 10:54:49 +0300

    init

--- app/src/main/java/com/nemo/bestpracticespartone/MainActivity.java
+++ app/src/main/java/com/nemo/bestpracticespartone/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void BIStart() {
-        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivityForResult(intent, 0);
         Toast.makeText(getApplicationContext(),"Turned on"
                 ,Toast.LENGTH_LONG).show();
